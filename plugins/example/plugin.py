48be14a8a08bc5f9813a227dff05baac6fdacbac
attv1arec-arch <attv1a.rec@gmail.com>
2026-09-11 20:59:10 -0400

    Delete plugins/example directory

--- plugins/example/plugin.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import json,sys
-from PIL import Image,ImageEnhance
-src,dst=sys.argv[1],sys.argv[2]
-opts=json.loads(sys.argv[3]) if len(sys.argv)>3 else {}
-im=Image.open(src).convert('RGB')
-im=ImageEnhance.Contrast(im).enhance(float(opts.get('contrast',1.08)))
-im.save(dst,quality=95)
